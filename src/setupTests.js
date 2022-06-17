164f3648fc5b73e5ab734901b49c989c09eafa90
Samuel Thng <zthng@ap.equinix.com>
2022-06-18 00:42:19 +0800

    Setup: Packages and Test Environment

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+beforeAll(() => {
+	global.matchMedia =
+	global.matchMedia ||
+	function () {
+		return {
+			matches: false,
+			addListener: jest.fn(),
+			removeListener: jest.fn(),
+		};
+	};
+});
